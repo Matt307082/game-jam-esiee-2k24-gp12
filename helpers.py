938ec54cb5a1099f2795a50b0239fdce35b20588
BUGES Romain <romain.buges@veolia.com>
2024-09-18 11:59:58 +0200

    Petite modification

--- helpers.py
+++ helpers.py
@@ -30,6 +30,5 @@ class SeasonManager:
 
     def draw(this, screen):
         for season in Season:
-            print(this.current_season, season)
             this.seasons_icons.set_alpha(255 if this.current_season.value==season.value else 100)
             screen.blit(this.seasons_icons, (60*season.value + 25, 25), ((season.value%2) * 52, (season.value//2) * 52, 50, 50))
