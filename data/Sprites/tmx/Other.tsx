71556c39805d56460e95b44f9a8e231e272790a5
_SportDay_ <49455066+SportDay@users.noreply.github.com>
2024-09-19 16:12:16 +0200

    add ice

--- data/Sprites/tmx/Other.tsx
+++ data/Sprites/tmx/Other.tsx
@@ -1,10 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Other" tilewidth="32" tileheight="32" tilecount="2" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="Other" tilewidth="32" tileheight="32" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="2">
   <image source="../star.png" width="32" height="32"/>
  </tile>
  <tile id="3">
   <image source="../platformPack_tile010.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="../iceBlock.png" width="32" height="32"/>
  </tile>
 </tileset>
