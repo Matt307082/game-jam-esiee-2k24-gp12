80a74fe982e8f021f5a251f24d5753121c6c56ef
BUGES Romain <romain.buges@veolia.com>
2024-09-19 15:14:02 +0200

    Réparation de la musique

--- Tools/MusicManager.py
+++ Tools/MusicManager.py
@@ -6,16 +6,14 @@ pygame.init()
 
 class MusicManager:
     files = dict()
-    current_music = ""
 
     def load_files(self, *args):
         for music_name in args:
             self.files[music_name] = open(f"data/soundtrack/{music_name}.mp3")
 
     def play(self, music_name):
-        if music_name in self.files and self.current_music != music_name:
-            self.current_music = music_name;
-            pygame.mixer.music.unload()
-            pygame.mixer.music.load(self.files[music_name])
-            pygame.mixer.music.play(-1, random.uniform(0, 300))
-            self.load_files(music_name)
+        print(music_name)
+        pygame.mixer.music.unload()
+        pygame.mixer.music.load(self.files[music_name])
+        pygame.mixer.music.play(-1, random.uniform(0, 300))
+        self.load_files(music_name)
