0ff1b117a5515d4dd3f3cde8233b9a75d72903cd
_SportDay_ <49455066+SportDay@users.noreply.github.com>
2024-09-19 14:45:21 +0200

    fix niveau1

--- main.py
+++ main.py
@@ -112,7 +112,6 @@ while not done:
                     GAME_STATE["nextLevel"] = True
                 if event.key == pygame.K_SPACE :
                     print(GAME_STATE['layer_obj'])
-                    print(GAME_STATE['layer_obj']['bear'])
 
             #vidange de la clef stocké
             if event.type == pygame.KEYUP:
